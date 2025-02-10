218469de9aeadf7c46783e876310df722b833601
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-10 13:24:23 -0300

    Criado BCryptPasswordEncoder nos Configs e injetado no TokenController , além de criar o método isLoginCorrect para verificar as senhas

--- src/main/java/com/twitter/config/SecurityConfig.java
+++ src/main/java/com/twitter/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -52,6 +53,11 @@ public class SecurityConfig {
         JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
         var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/twitter/controller/TokenController.java
+++ src/main/java/com/twitter/controller/TokenController.java
@@ -2,7 +2,10 @@ package com.twitter.controller;
 
 import com.twitter.controller.dto.LoginRequest;
 import com.twitter.controller.dto.LoginResponse;
+import com.twitter.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +16,23 @@ public class TokenController {
 
     private final JwtEncoder jwtEncoder;
 
-    public TokenController(JwtEncoder jwtEncoder) {
+    private final UserRepository userRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public TokenController(JwtEncoder jwtEncoder, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest) {
-        
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+
+        var user = userRepository.findByUsername(loginRequest.username());
+
+        if (user.isEmpty() || user.get().isLoginCorrect(loginRequest, bCryptPasswordEncoder)) {
+            throw new BadCredentialsException("user or password is invalid!");
+        }
     }
 }
