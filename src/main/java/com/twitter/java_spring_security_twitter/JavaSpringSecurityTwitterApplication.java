db46bf537c2976f4a3734fe5f7e691759171d708
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-10 08:21:00 -0300

    Criando os arquivos base

---
+++ src/main/java/com/twitter/java_spring_security_twitter/JavaSpringSecurityTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.java_spring_security_twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSecurityTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSecurityTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitter/java_spring_security_twitter/JavaSpringSecurityTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.java_spring_security_twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSecurityTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
