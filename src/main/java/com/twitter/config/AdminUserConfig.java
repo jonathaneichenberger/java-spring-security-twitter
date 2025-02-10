4bde5aa6e181dcbb70a6e2859af8dafac8df70a3
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-10 13:47:06 -0300

    Adicionando AdminUserConfig e configurando a criação de user admin

---
+++ src/main/java/com/twitter/config/AdminUserConfig.java
@@ -0,0 +1,54 @@
+package com.twitter.config;
+
+
+import com.twitter.entities.Role;
+import com.twitter.entities.User;
+import com.twitter.repository.RoleRepository;
+import com.twitter.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private UserRepository userRepository;
+
+    private RoleRepository roleRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public AdminUserConfig(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+
+
+        var roleAdmin =  roleRepository.findByName(Role.Values.ADMIN.name());
+
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> {
+                    System.out.println("admin já existe");
+                },
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(bCryptPasswordEncoder.encode("123"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+                }
+        );
+    }
+}
