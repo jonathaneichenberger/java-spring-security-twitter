389212c6c3021091ce83b2c5d1cc86435f75facc
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-10 13:32:45 -0300

    Update TokenController.java

--- src/main/java/com/twitter/controller/TokenController.java
+++ src/main/java/com/twitter/controller/TokenController.java
@@ -6,10 +6,14 @@ import com.twitter.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 @RestController
 public class TokenController {
@@ -31,8 +35,23 @@ public class TokenController {
 
         var user = userRepository.findByUsername(loginRequest.username());
 
-        if (user.isEmpty() || user.get().isLoginCorrect(loginRequest, bCryptPasswordEncoder)) {
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, bCryptPasswordEncoder)) {
             throw new BadCredentialsException("user or password is invalid!");
         }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("mybackend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok( new LoginResponse(jwtValue, expiresIn));
+
     }
 }
