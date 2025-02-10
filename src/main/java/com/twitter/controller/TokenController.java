b299af630df092a16f4193797d10f6b7ea61c604
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-10 19:26:30 -0300

    Atualizações de permissões de acesso para poder listar todos usuarios apenas com perfil de admin

--- src/main/java/com/twitter/controller/TokenController.java
+++ src/main/java/com/twitter/controller/TokenController.java
@@ -2,6 +2,7 @@ package com.twitter.controller;
 
 import com.twitter.controller.dto.LoginRequest;
 import com.twitter.controller.dto.LoginResponse;
+import com.twitter.entities.Role;
 import com.twitter.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
@@ -42,11 +44,17 @@ public class TokenController {
         var now = Instant.now();
         var expiresIn = 300L;
 
+        var scopes = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+
         var claims = JwtClaimsSet.builder()
                 .issuer("mybackend")
                 .subject(user.get().getUserId().toString())
                 .issuedAt(now)
                 .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
                 .build();
 
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
