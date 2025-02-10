ce69c518f2074002456e89b183f143a548b0b7cd
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-10 12:58:16 -0300

    Gerando as chaves pública e privada e adicionando as properties

--- src/main/java/com/twitter/config/SecurityConfig.java
+++ src/main/java/com/twitter/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.twitter.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,9 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
 
     @Bean
     private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,8 +30,10 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        
+
         return http.build();
     }
+
+    
 
 }
