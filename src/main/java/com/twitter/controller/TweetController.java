cda303b694949db235ed2b30ea9926f64b220384
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-11 11:42:05 -0300

    Resolvido erro do retorno 403 forbidden(foi problema no Postman), Criado FeedDto, FeedItemDto e terminado o Projeto com TweetController acabado.

--- src/main/java/com/twitter/controller/TweetController.java
+++ src/main/java/com/twitter/controller/TweetController.java
@@ -1,12 +1,20 @@
 package com.twitter.controller;
 
 import com.twitter.controller.dto.CreateTweetDto;
+import com.twitter.controller.dto.FeedDto;
+import com.twitter.controller.dto.FeedItemDto;
+import com.twitter.entities.Role;
 import com.twitter.entities.Tweet;
 import com.twitter.repository.TweetRepository;
 import com.twitter.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 
 import java.util.UUID;
 
@@ -20,6 +28,23 @@ public class TweetController {
                            UserRepository userRepository) {
         this.tweetRepository = tweetRepository;
         this.userRepository = userRepository;
+    }
+
+    @GetMapping("/feed")
+    public ResponseEntity<FeedDto> feed(@RequestParam(value = "page", defaultValue = "0") int page,
+                                        @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+
+        var tweets = tweetRepository.findAll(
+                        PageRequest.of(page, pageSize, Sort.Direction.DESC, "creationTimestamp"))
+                .map(tweet ->
+                        new FeedItemDto(
+                                tweet.getTweetId(),
+                                tweet.getContent(),
+                                tweet.getUser().getUsername())
+                );
+
+        return ResponseEntity.ok(new FeedDto(
+                tweets.getContent(), page, pageSize, tweets.getTotalPages(), tweets.getTotalElements()));
     }
 
     @PostMapping("/tweets")
@@ -36,4 +61,25 @@ public class TweetController {
         return ResponseEntity.ok().build();
     }
 
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
+                                            JwtAuthenticationToken token) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get().getRoles()
+                .stream()
+                .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+            tweetRepository.deleteById(tweetId);
+
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+
+        return ResponseEntity.ok().build();
+    }
 }
---
+++ src/main/java/com/twitter/controller/dto/FeedItemDto.java
@@ -0,0 +1,4 @@
+package com.twitter.controller.dto;
+
+public record FeedItemDto(long tweetId, String content, String username) {
+}
