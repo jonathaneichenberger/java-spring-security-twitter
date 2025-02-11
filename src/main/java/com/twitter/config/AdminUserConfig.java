7ccd0425f569c3554eaecdb5d4f6bd0b760c108f
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-11 10:59:28 -0300

    Revisando AdminUserConfig

--- src/main/java/com/twitter/config/AdminUserConfig.java
+++ src/main/java/com/twitter/config/AdminUserConfig.java
@@ -1,51 +1,50 @@
 package com.twitter.config;
 
-
 import com.twitter.entities.Role;
 import com.twitter.entities.User;
 import com.twitter.repository.RoleRepository;
 import com.twitter.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.Set;
 
 @Configuration
 public class AdminUserConfig implements CommandLineRunner {
 
-    private UserRepository userRepository;
-
     private RoleRepository roleRepository;
 
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private UserRepository userRepository;
+    
+    private BCryptPasswordEncoder passwordEncoder;
 
-    public AdminUserConfig(UserRepository userRepository,
-                           RoleRepository roleRepository,
-                           BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userRepository = userRepository;
+    public AdminUserConfig(RoleRepository roleRepository,
+                           UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
         this.roleRepository = roleRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     @Transactional
     public void run(String... args) throws Exception {
 
-
-        var roleAdmin =  roleRepository.findByName(Role.Values.ADMIN.name());
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
 
         var userAdmin = userRepository.findByUsername("admin");
 
         userAdmin.ifPresentOrElse(
                 user -> {
-                    System.out.println("admin já existe");
+                    System.out.println("admin ja existe");
                 },
                 () -> {
                     var user = new User();
                     user.setUsername("admin");
-                    user.setPassword(bCryptPasswordEncoder.encode("123"));
+                    user.setPassword(passwordEncoder.encode("123"));
                     user.setRoles(Set.of(roleAdmin));
                     userRepository.save(user);
                 }
